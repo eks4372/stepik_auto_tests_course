9936f525cfce4d4fb47e49354cd5c2cccceb689d
kosten <mail@mail.ru>
2022-10-05 14:34:24 +0500

    add support lng

--- test_items.py
+++ test_items.py
@@ -1,10 +1,12 @@
 from selenium.webdriver.common.by import By
+import time
 
 link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
 
 def test_button_present(browser):
     browser.get(link)
-    assert browser.find_element(By.CSS_SELECTOR, '[value="Добавить в корзину"]'), 'нет кнопки "Добавить в корзину"'
+    time.sleep(5)
+    assert browser.find_element(By.CLASS_NAME, 'btn-add-to-basket'), 'нет кнопки "Добавить в корзину"'
 
 # pytest --language=es test_items.py
